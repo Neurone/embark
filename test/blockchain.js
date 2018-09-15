f420b2620e426a51cf8a361110582525a042823b
Neurone <bertone.giuseppe@gmail.com>
2018-09-15 23:32:42 +0200

    WIP #773: - Fixed blockchain test; - Clients are now separated; - Fixed i18n strings;

--- test/blockchain.js
+++ test/blockchain.js
@@ -1,23 +1,22 @@
 /*globals describe, it*/
-const Blockchain =  require('../lib/modules/blockchain_process/blockchain.js');
+const Blockchain = require('../lib/modules/blockchain_process/blockchain.js');
 const constants = require('../lib/constants.json');
 const {defaultHost} = require('../lib/utils/host');
 
 const assert = require('assert');
 
-describe('embark.Blockchain', function () {
-  //let Client = function() {};
-  //Client.prototype.name = "ClientName";
+describe('embark.Blockchain', function() {
 
-  describe('#initializer', function () {
-    //let client = new Client();
+  describe('#initializer', function() {
 
-    describe('with empty config', function () {
-      it('should have a default config', function (done) {
+    describe('with empty config', function() {
+      it('should have a default config', function(done) {
+        let blockchain = new Blockchain({});
         let expectedConfig = {
           networkType: 'custom',
           genesisBlock: false,
-          geth_bin: 'geth',
+          ethereumClientName: 'geth',
+          ethereumClientBin: 'geth',
           datadir: false,
           mineWhenNeeded: false,
           rpcHost: defaultHost,
@@ -42,10 +41,10 @@ describe('embark.Blockchain', function () {
           syncmode: undefined,
           syncMode: undefined,
           verbosity: undefined,
-          proxy: true
+          proxy: true,
+          silent: undefined
         };
-        let blockchain = new Blockchain({}, 'geth');
-
+        // We check also proxy's ports because proxy is set to true
         expectedConfig.wsPort += constants.blockchain.servicePortOnProxy;
         expectedConfig.rpcPort += constants.blockchain.servicePortOnProxy;
 
@@ -55,12 +54,13 @@ describe('embark.Blockchain', function () {
       });
     });
 
-    describe('with config', function () {
-      it('should take config params', function (done) {
+    describe('with config', function() {
+      it('should take config params', function(done) {
         let config = {
           networkType: 'livenet',
           genesisBlock: 'foo/bar/genesis.json',
-          geth_bin: 'geth',
+          ethereumClientName: 'geth',
+          ethereumClientBin: 'geth',
           datadir: '/foo/datadir/',
           mineWhenNeeded: true,
           rpcHost: defaultHost,
@@ -87,14 +87,45 @@ describe('embark.Blockchain', function () {
           verbosity: undefined,
           proxy: true
         };
-        let blockchain = new Blockchain(config, 'geth');
+        let blockchain = new Blockchain(config);
 
-        if(config.proxy){
-          config.wsPort += constants.blockchain.servicePortOnProxy;
-          config.rpcPort += constants.blockchain.servicePortOnProxy;
-        }
+        let expectedConfig = {
+          networkType: 'livenet',
+          genesisBlock: 'foo/bar/genesis.json',
+          ethereumClientName: 'geth',
+          ethereumClientBin: 'geth',
+          datadir: '/foo/datadir/',
+          mineWhenNeeded: true,
+          rpcHost: defaultHost,
+          rpcPort: 12345,
+          rpcApi: ['eth', 'web3', 'net', 'debug'],
+          rpcCorsDomain: true,
+          networkId: 1,
+          port: 123456,
+          nodiscover: true,
+          maxpeers: 25,
+          mine: true,
+          vmdebug: false,
+          whisper: false,
+          account: {},
+          bootnodes: "",
+          wsApi: ["eth", "web3", "net", "shh", "debug"],
+          wsHost: defaultHost,
+          wsOrigins: false,
+          wsPort: 12346,
+          wsRPC: true,
+          targetGasLimit: false,
+          syncMode: undefined,
+          syncmode: undefined,
+          verbosity: undefined,
+          proxy: true,
+          silent: undefined
+        };
+        // We check also proxy's ports because proxy is set to true
+        expectedConfig.wsPort += constants.blockchain.servicePortOnProxy;
+        expectedConfig.rpcPort += constants.blockchain.servicePortOnProxy;
 
-        assert.deepEqual(blockchain.config, config);
+        assert.deepEqual(blockchain.config, expectedConfig);
         blockchain.kill();
         done();
       });
